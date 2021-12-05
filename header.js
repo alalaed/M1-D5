d09af20f359e864fa47effeb6a9aaffc45821dd0
Ali Alaeddine <a.alaeddine@live.com>
2021-12-05 12:16:20 +0100

    modified header

--- header.js
+++ header.js
@@ -1,15 +1,15 @@
 
 
-let header = ""  
-let headerIndent = "" // will be the string with the dashes inserted before and after a given title
-let lengthOfHeader = 50  // determines how long is the header; input based
-let title = ""
+
+
 
 
 const finalHeader = function(title){
-    
+    let header = ""  
+    let headerIndent = "" // will be the string with the dashes inserted before and after a given title
     let desiredCharacter = "-"
-    let titleModified = "== " + title + " =="  
+    let titleModified = "== " + title + " ==" 
+    let lengthOfHeader = 50  // determines how long is the header; input based
     let numberOfDashes = (lengthOfHeader - titleModified.length) / 2 // determines how many dashes should be inserted based on the length of the title
 
     for (i = 0; i < numberOfDashes; i++){
@@ -24,6 +24,8 @@ const finalHeader = function(title){
 
 }
 
+finalHeader("this is a title")
+finalHeader("this is a title")
 finalHeader("this is a title")
 
 
